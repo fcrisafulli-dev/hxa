11bd3e4bc8ef8bb21bca2d2136f3b7793585ac65
fcrisafulli-dev <email@email.email>
2023-03-10 14:21:04 -0500

    change name because of convention

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{BufReader, Seek};
-mod hxa;
+use hxa;
 
 fn main() {
     println!("Opening file...");
@@ -15,7 +15,6 @@ fn main() {
     hxa_file.read_header(&mut input);
 
     //dbg!(hxa_file);
-
 
     let file_location = input.seek(std::io::SeekFrom::Current (0)).expect("Could not get current position!");
     println!("{:X?}",file_location);
